0990cb55ba9f63cf418867003de23f117a1bc72d
roman <sharypovroman92@gmail.com>
2023-04-21 03:47:34 +0300

    add lecture 003

---
+++ lectures/lecture_003/main.py
@@ -0,0 +1,15 @@
+# def sum_numbers(n):
+#     sum = 0
+#     for i in range(n + 1):
+#         sum += i
+#     print(sum)
+
+# sum_numbers(5)
+
+
+def sum_numbers(n):
+    sum = 0
+    for i in range(n + 1):
+        sum += i
+    return sum
+print(sum_numbers(5))
